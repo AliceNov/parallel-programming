892b23dc2394cf7a7148f2da41da17eddfbe48f5
Новикова Алиса Александровна <novikova.a@ccam.cmc.unn.net>
2018-11-01 10:45:09 +0300

    Update1

--- 1608/Novikova_AA/main.cpp
+++ 1608/Novikova_AA/main.cpp
@@ -1,4 +1,5 @@
-﻿#include "mpi.h"
+#define _CRT_NO_WARNINGS
+#include "mpi.h"
 #include <time.h>
 #include <iostream>
 
@@ -6,134 +7,133 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int *array = nullptr, *tarray = nullptr;;
-	int ProcNum, ProcRank;//Число и ранг процессов
-	int N = 0, M = 0; // Колонки и строки 
-	int *result = nullptr; 	// Результат 
+	int *matrix = nullptr, *tarray = nullptr;
+	int ProcNum, ProcRank;
+	int N = 0, M = 0;
+	int *result = nullptr;
 	MPI_Status status;
-	double times;
-	int k = 0, l = 0;
+	double t;
+	int h = 0, v = 0;
 
-	/*Начало MPI кода*/
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 
-	if (ProcRank == 0) 
+	if (ProcRank == 0)
 	{
-		if (argc < 3)
-		{
-		cout << "Enter number of colums = ";
-		cin >> N;
-		cout << "Enter number of rows = ";
-		cin >> M;
-		}
-		else
-		{
-			N = atoi(argv[1]);
-			M = atoi(argv[2]);
-		}
-
-		array = new int[N*M];
+		N = atoi(argv[1]);
+		M = atoi(argv[2]);
+	
+		matrix = new int[N*M];
 		srand(time(0));
-		//Заполняем матрицу рандомными числами 
-		for (int i = 0; i<N*M; i++)
-			array[i] = rand() % 10;
+		for (int i = 0; i < N*M; i++)
+			matrix[i] = rand() % 10;
 
 		result = new int[M];
-		for (int i = 0; i< M; i++)
+		for (int i = 0; i < M; i++)
 			result[i] = 0;
 
 		tarray = new int[N*M];
 		int h = 0;
-		for (int j = 0; j < M; j++){
-			for (int i = j; i < N*M; i+=M){
-				tarray[h] = array[i];
+		for (int j = 0; j < M; j++)
+			for (int i = j; i < N*M; i += M)
+			{
+				tarray[h] = matrix[i];
 				h++;
 			}
-		}
-
 	}
-	
-	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD); //передаем размерности всем процессам
-	MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);//передаем размерности всем процессам
+
+
+	MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (ProcRank == 0)
 	{
-		
 
-		k = M / ProcNum; //количество строк для каждого процесса
-		l = M % ProcNum; //оставшееся количество, первому процессу
 
-		//делим строки матрицы 
-		for (int i = 1; i< ProcNum; i++)
-			MPI_Send(tarray + (i)*k*N, k*N, MPI_INT, i, i, MPI_COMM_WORLD);
+		h = M / ProcNum; //���������� ����� ��� ������� ��������
+		v = M % ProcNum; //���������� ����������, ������� ��������
+		t = MPI_Wtime();
+		for (int i = 1; i < ProcNum; i++)
+			MPI_Send(tarray + (i)*h*N, h*N, MPI_INT, i, i, MPI_COMM_WORLD);
 	}
 
-	times = MPI_Wtime();
-	//вычисляем суммы
 	if (ProcRank != 0)
 	{
-		k = M / ProcNum;
-		int *tmp = new int[N*k];
-		int *res = new int[k];
+		h = M / ProcNum;
+		int *tmp = new int[N*h];
+		int *res = new int[h];
 		int sum;
 
-		MPI_Recv(tmp, N*k, MPI_INT, 0, ProcRank, MPI_COMM_WORLD, &status);
+		MPI_Recv(tmp, N*h, MPI_INT, 0, ProcRank, MPI_COMM_WORLD, &status);
 
-		for (int i = 0; i < k; i++)
+		for (int i = 0; i < h; i++)
 		{
 			sum = 0;
 			for (int j = 0; j < N; j++)
-				sum += tmp[j + N*i];
+				sum += tmp[j + N * i];
 			res[i] = sum;
 		}
-		MPI_Send(res, k, MPI_INT, 0, ProcRank, MPI_COMM_WORLD);
+		MPI_Send(res, h, MPI_INT, 0, ProcRank, MPI_COMM_WORLD);
 		delete tmp;
 		delete res;
 	}
 	else
 	{
 		int sum;
-		for (int i = 0; i < k; i++)
+		for (int i = 0; i < h; i++)
 		{
 			sum = 0;
-			for (int j = 0; j<N; j++)
-				sum += tarray[j + i*N];
+			for (int j = 0; j < N; j++)
+				sum += tarray[j + i * N];
 			result[i] = sum;
 		}
 
-		if (l != 0)
+		if (v != 0)
 		{
-			for (int i = 0; i < l; i++)
+			for (int i = 0; i < v; i++)
 			{
 				sum = 0;
 				for (int j = 0; j < N; j++)
-					sum += tarray[k*ProcNum*N + j + i*N];
-				result[i + k*ProcNum] = sum;
+					sum += tarray[h*ProcNum*N + j + i * N];
+				result[i + h * ProcNum] = sum;
 			}
 		}
 	}
 	
-	printf("Time = %.10f\n", MPI_Wtime() - times);
-
 	if (ProcRank == 0)
 	{
-		for (int i = 1; i< ProcNum; i++)
+		for (int i = 1; i < ProcNum; i++)
 		{
-			MPI_Recv(result + i*k, k, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);
+			MPI_Recv(result + i * h, h, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);
 		}
 		cout<<"\n";
-		for (int i = 0; i<M; i++)
+		for (int i = 0; i < N; i++)
 		{
-			for (int j = 0; j<N; j++)
+			for (int j = 0; j < M; j++)
 			{
-				cout << tarray[i*M + j] << " ";
+				cout<< matrix[i*M + j]<<" ";
 			}
-			cout << "sum = " << result[i]<<"\n";
+			cout<<"\n";
 		}
+
+		for (int j = 1; j <= M; j++)
+		{
+			cout<<"Sum of "<<j<<" column: "<< result[j - 1];
+			cout << "\n";
+		}
+		cout << "Time = " << MPI_Wtime() - t;
+		cout << "\n";
+
 	}
+
+	if (matrix != nullptr)
+		delete matrix;
+	if (tarray != nullptr)
+		delete tarray;
+	if (result != nullptr)
+		delete result;
 
 	MPI_Finalize();
 	return 0;
